6cd7a4443e4cc175cb86e244f5efb57589a22cf4
Bruno Frank <bfscordeiro@gmail.com>
2018-07-25 22:33:07 -0300

    Update PhotoLibrary.swift

--- src/ios/PhotoLibrary.swift
+++ src/ios/PhotoLibrary.swift
@@ -117,7 +117,7 @@ import Foundation
             let options = command.arguments[1] as! NSDictionary
             let thumbnailWidth = options["thumbnailWidth"] as! Int
             let thumbnailHeight = options["thumbnailHeight"] as! Int
-            let quality = options["quality"] as! Float
+            let quality = (options["quality"] as? NSNumber)?.floatValue as! Float
 
             service.getThumbnail(photoId, thumbnailWidth: thumbnailWidth, thumbnailHeight: thumbnailHeight, quality: quality) { (imageData) in
 
